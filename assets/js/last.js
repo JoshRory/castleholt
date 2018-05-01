b2300cb6234fb32c3cb626a9d6e91f2eab4de3f0
Lewis Matthews <Lewis.Matthews@argos.co.uk>
2018-05-01 09:49:32 +0100

    more gallery tweaks

---
+++ assets/js/last.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    if($("#lightSlider").length){
+        $("#lightSlider").lightSlider({
+            item: 1,
+            controls: true
+        });     
+    }
+});
